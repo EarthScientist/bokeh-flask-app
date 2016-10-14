5c1e6678f823ddc0aaabb68e6c253e1cf4640675
EarthScientist <lindgren.mike@gmail.com>
2016-10-14 14:07:47 -0800

    make run.py runnable again following the procfile snafus (still not solved.)

--- run.py
+++ run.py
@@ -1,8 +1,4 @@
 #!flask/bin/python
 from app import app
-if __name__ == "__main__":
-	import os
-    port = int(os.environ.get("PORT", 5000))
-    # app.run()
-	app.run( host='0.0.0.0', port=port, debug=False )
+app.run( debug=False )
 
