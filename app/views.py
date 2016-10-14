c67052cd0395a08115bd5ff7ca09a3e9ef914f5f
EarthScientist <lindgren.mike@gmail.com>
2016-10-14 14:10:02 -0800

    removes some old commented code

--- app/views.py
+++ app/views.py
@@ -1,14 +1,3 @@
-# from flask import render_template
-# from app import app
-
-# @app.route('/')
-# @app.route('/index')
-# def index():
-#     user = {'nickname': 'Miguel'}  # fake user
-#     return render_template('index.html',
-#                            title='Home',
-#                            user=user)
-
 from flask import render_template
 from app import app
 
