e17d2f5b7f36bde9113d238986fac691bcb214bb
EarthScientist <lindgren.mike@gmail.com>
2016-12-11 23:58:02 -0900

    wires up (clunkily) way to use simple drop-down menu to examine different variables.

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,11 @@
-# # # # NEW FORM TESTING 
 from flask_wtf import Form
-from wtforms import TextField, IntegerField, TextAreaField, SubmitField, RadioField, SelectField
-from wtforms import validators, ValidationError
+from wtforms import SelectField
+# from wtforms import validators, ValidationError
 
-class ContactForm( Form ):
-    language = SelectField( 'Languages', choices = [('cpp', 'C++'), ('py', 'Python')] )
-    submit = SubmitField( "Send" )
-
-# # # # END NEW FORM TESTING 
+class DropDown( Form ):
+    cols = SelectField( 'Columns' )
+    
+    def __init__( self, *args, **kwargs ):
+        super( DropDown, self ).__init__( *args, **kwargs )
+        colnames = [ u'High', u'Low', u'Close', u'Adj. Volume' ]
+        self.cols.choices = zip( range( len( colnames ) ), colnames )
--- app/views.py
+++ app/views.py
@@ -1,10 +1,8 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
-from flask import Flask, render_template, request, flash
-from forms import ContactForm
 
 
-def make_figure():
+def make_figure( col ):
     import requests
     from bokeh.plotting import figure, show
     from bokeh.embed import components
@@ -16,7 +14,7 @@ def make_figure():
     api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json' % stock
     session = requests.Session()
     session.mount( 'http://', requests.adapters.HTTPAdapter( max_retries=3 ) )
-    raw_data = session.get(api_url)
+    raw_data = session.get( api_url )
 
     # wrangle
     dat = raw_data.json()
@@ -29,12 +27,16 @@ def make_figure():
                    x_axis_label='date',
                    x_axis_type='datetime' )
 
-    plot.line( pd.to_datetime( df.get( 'Date' ) ), df.get( 'High' ), color='#A6CEE3', legend=stock )
+    if col is None:
+        col = 'High'
+    
+    print( col + 'make' )
+    plot.line( pd.to_datetime( df.get( 'Date' ) ), df.get( col ), color='#A6CEE3', legend=stock )
     # plot.line( np.array(df.get( 'Date' )), np.array(df.get( 'High' )), color='#A6CEE3', legend=stock )
     return plot
 
 @app.route('/')
-@app.route('/index',  methods = ['GET', 'POST'])
+@app.route('/index',  methods=['GET', 'POST'])
 def index():
     import requests
     from bokeh.plotting import figure, show
@@ -42,36 +44,33 @@ def index():
     from bokeh.resources import INLINE
     import pandas as pd
     import numpy as np
-
-    plot = make_figure()
-    header = 'AAPL Highs'
-
-    script, div = components( plot )
+    from forms import DropDown
     
-    # # # FORM
-    form = ContactForm()
-    return render_template('index.html', script=script, div=div, header=header, form=form )
+    form = DropDown()
 
-    # if request.method == 'POST':
-        # if form.validate() == False:
-            # flash('All fields are required.')
+    if request.method == 'POST':
+        print( 'post' )
+        col = form.data[ 'cols' ]
+        print( col )
+        col_id, col_name = form.cols.choices[ int(col) ]
+        print( col_name )
 
-    # # # END FORM 
+        plot = make_figure( col=col_name )
+        header = 'AAPL {}'.format( col_name )
+
+        script, div = components( plot )
+        
+        return render_template('index.html', script=script, div=div, header=header, form=form )
+    elif request.method == 'GET':
+        print( 'get' )
+        col_name = None # tmp
+        plot = make_figure( col=col_name )
+        header = 'AAPL {}'.format( col_name )
+
+        script, div = components( plot )
+        
+        return render_template('index.html', script=script, div=div, header=header, form=form )
+    else:
+        BaseException( 'error' )
 
 
-
-
-
-
-# @app.route('/contact',)
-# def contact():
-#    form = ContactForm()
-   
-#    if request.method == 'POST':
-#       if form.validate() == False:
-#          flash('All fields are required.')
-#          return render_template('contact.html', form = form)
-#       else:
-#          return render_template('success.html')
-#       elif request.method == 'GET':
-#          return render_template('contact.html', form = form)
