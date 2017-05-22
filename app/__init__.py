1ebbbd8b2d8039b5ecbc2b129aacad6439c5031a
EarthScientist <lindgren.mike@gmail.com>
2017-05-22 05:22:39 -0800

    add "real" data

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.secret_key = 'development key'
+app.secret_key = 'development key' # this is dangerTown but ok for testing internally
 from app import views
